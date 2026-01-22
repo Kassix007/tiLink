8b1af9c8ae8f5126a44ed4ea7f5820f06209ab8c
Kassix007 <shaankassi@gmail.com>
2026-01-22 23:39:14 +0400

    added header

---
+++ UI/src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header: React.FC = () => {
+    return (
+        <header className="header">
+            <div className="header-content">
+                <h1>Ti Link</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
