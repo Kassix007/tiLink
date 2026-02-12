700dd46b78e5aae3417dc06225c81c30a149a9cc
yajnesh11@outlook.com <yajnesh11@outlook.com>
2026-02-12 21:39:39 +0400

    Playwright Automation

--- Automation/Tests/api/health.spec.ts
+++ Automation/Tests/api/health.spec.ts
@@ -1,4 +1,6 @@
-﻿import { test, expect } from '@playwright/test';
+﻿//npx playwright test --headed -> use to run tests in terminal
+
+import { test, expect } from '@playwright/test';
 
 test('API is reachable', async ({ request }) => {
     const response = await request.get('http://localhost:5000/swagger/index.html');
